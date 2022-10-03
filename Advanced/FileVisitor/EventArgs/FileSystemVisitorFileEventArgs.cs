80a17e68a17fa23cfc27846ef7324072ff4bbc71
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-03 22:34:42 +0200

    refactor: minor refactor

--- Advanced/FileVisitor/EventArgs/FileSystemVisitorFileEventArgs.cs
+++ Advanced/FileVisitor/EventArgs/FileSystemVisitorFileEventArgs.cs
@@ -1,8 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace FileVisitor
 {
-    public class FileSystemVisitorFileEventArgs
+    public class FileSystemVisitorFileEventArgs : EventArgs
     {
         public FileInfo FileInfo { get; set; }
     }
