e5f07b3a6c7648344ef8ebf81ab3c8a2b73b0fa3
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-03 14:25:19 +0200

    feat: complete the first task

---
+++ Fundamentals/Fundamentals.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Fundamentals.Standard;
+
+namespace Fundamentals.Core
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException($"Expected one command line parameter, but was {args.Length}.");
+            }
+
+            var username = args[0];
+            Console.WriteLine(Concatenator.GetGreetingMessage(username));
+        }
+    }
+}
---
+++ Fundamentals/Fundamentals.Standard/Concatenator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentals.Standard
+{
+    public static class Concatenator
+    {
+        public static string GetGreetingMessage(string username)
+        {
+            return $"{DateTime.Now.ToString("HH:mm")} Hello, {username}!";
+        }
+    }
+}
