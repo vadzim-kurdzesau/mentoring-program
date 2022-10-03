0a5d1d2f43e6422b3cd9976054c99b1f7d43137b
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-03 17:36:56 +0200

    feat: implement console demo

---
+++ Advanced/FileVisitor.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FileVisitor.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("Provide path to the root directory.");
+
+            var directoryPath = args[0];
+            var fileSystemVisitor = new FileSystemVisitor();
+            var content = fileSystemVisitor.GetDirectoryContent(directoryPath);
+
+            foreach (var item in content)
+            {
+                Console.WriteLine(item.FullName);
+            }
+        }
+    }
+}
