cf88d119738ad555c06380c24622ff828299ef90
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-05 19:24:56 +0200

    feat: finish the first task

--- Exception Handling/Task1/Program.cs
+++ Exception Handling/Task1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            while (true)
+            {
+                try
+                {
+                    var input = Console.ReadLine();
+                    Console.WriteLine(input[0]);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Do not enter the empty lines");
+                }
+            }
         }
     }
 }
