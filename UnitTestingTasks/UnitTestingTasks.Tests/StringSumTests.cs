f7d072c62d34c2a8daba948af09d95a3174dd4a8
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-08 16:10:29 +0200

    Add empty string test

---
+++ UnitTestingTasks/UnitTestingTasks.Tests/StringSumTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestingTasks.Tests
+{
+    public class StringSumTests
+    {
+        [Fact]
+        public void Sum_EmptyStrings_CountThemAsZeros()
+        {
+            const string expected = "0";
+
+            var actual = StringSum.Sum(string.Empty, string.Empty);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
