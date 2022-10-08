4cca374b62a2e205da8c8594a3384434814da57b
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-08 16:15:07 +0200

    Add small numbers tests

--- UnitTestingTasks/UnitTestingTasks.Tests/StringSumTests.cs
+++ UnitTestingTasks/UnitTestingTasks.Tests/StringSumTests.cs
@@ -18,5 +18,26 @@ namespace UnitTestingTasks.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("1", "2", "3")]
+        [InlineData("2", "2", "4")]
+        public void Sum_NatualNumbers_SumsThem(string firstNum, string secondNum, string expected)
+        {
+            var actual = StringSum.Sum(firstNum, secondNum);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("-1", "2", "2")]
+        [InlineData("1.24", "3.14", "0")]
+        [InlineData("3.14", "5", "5")]
+        public void Sum_NotNatualNumbers_SumsThem(string firstNum, string secondNum, string expected)
+        {
+            var actual = StringSum.Sum(firstNum, secondNum);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
