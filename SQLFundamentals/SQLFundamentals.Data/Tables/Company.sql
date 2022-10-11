e25cd2f6a4fe1fab17cfc9dfa07e4190dfeb7c17
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-11 13:54:53 +0200

    refactor: move to dbo folder

--- SQLFundamentals/SQLFundamentals.Data/Tables/Company.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Company]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[Name] NVARCHAR(20) NOT NULL,
-	[AddressId] INT NOT NULL,
-	CONSTRAINT [Company_ToAddress] FOREIGN KEY (AddressId) REFERENCES [Address]([Id])
-)
--- SQLFundamentals/SQLFundamentals.Data/Tables/Person.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Person]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[FirstName] NVARCHAR(50) NOT NULL,
-	[LastName] NVARCHAR(50) NOT NULL
-)
