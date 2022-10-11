3c51ae569bc4b91a795a4d60dc5ce9834b143810
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-11 13:55:58 +0200

    feat: add view

---
+++ SQLFundamentals/SQLFundamentals.Data/dbo/Views/EmployeeInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS 
+	SELECT 
+		n.Id,
+		ISNULL(EmployeeName, CONCAT(FirstName, ' ', LastName)) AS EmployeeFullName,
+		CONCAT(ZipCode, '_', State, ',', City, '-', Street) AS EmployeeFullAddress,
+		CONCAT(CompanyName, '(', Position, ')') AS EmployeeCompanyInfo
+	FROM Employee AS n
+	LEFT JOIN Address AS a ON n.AddressId=a.Id
+	LEFT JOIN Person AS p ON n.PersonId=p.Id
+	ORDER BY CompanyName, City OFFSET 0 ROWS
