0a51c057996827055baa4468abc453e130b768af
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-11 13:54:33 +0200

    refactor: make primary keys auto-incrementing

---
+++ SQLFundamentals/SQLFundamentals.Data/dbo/Tables/Company.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Company]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL,
+	[AddressId] INT NOT NULL,
+	CONSTRAINT [Company_ToAddress] FOREIGN KEY (AddressId) REFERENCES [Address]([Id])
+)
