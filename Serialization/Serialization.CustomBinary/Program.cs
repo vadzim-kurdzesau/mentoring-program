c6aca8d493aa44f99f05639f995a486c46a051da
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-14 19:16:42 +0200

    feat: implement second serialization task

---
+++ Serialization/Serialization.CustomBinary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization.CustomBinary
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Arrange
+            const string serializationFileName = "CustomSerializedXmlDepartment.bin";
+            var user = new User
+            {
+                Name = "Vadzim",
+                BirthDay = new DateTime(2003, 2, 12)
+            };
+
+            // Act
+            using (var fileStream = new FileStream(serializationFileName, FileMode.Create))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(fileStream, user);
+            }
+
+            // Assert
+            using (var fileStream = new FileStream(serializationFileName, FileMode.Open))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                var actual = (User)binaryFormatter.Deserialize(fileStream);
+                if (!user.Equals(actual))
+                {
+                    throw new Exception("Deserialized object does not equal the original.");
+                }
+            }
+        }
+    }
+}
---
+++ Serialization/Serialization.CustomBinary/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization.CustomBinary
+{
+    [Serializable]
+    public class User : ISerializable, IEquatable<User>
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            // Reset the property value using the GetValue method.
+            Name = (string)info.GetValue("name", typeof(string));
+            BirthDay = (DateTime)info.GetValue("birthday", typeof(DateTime));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("name", Name, typeof(string));
+            info.AddValue("birthday", BirthDay, typeof(DateTime));
+        }
+
+        public bool Equals(User? other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return (this, other) switch
+            {
+                (_, null) => false,
+                _ => Name.Equals(other.Name)
+                    && BirthDay.Equals(other.BirthDay),
+            };
+        }
+    }
+}
