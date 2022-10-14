2d9b8f6029071343ba0cfbb5b94f23277b1261bb
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-14 18:15:34 +0200

    feat:  add classes for serialization

---
+++ Serialization/SerializationTask.Json/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationTask.Json
+{
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
