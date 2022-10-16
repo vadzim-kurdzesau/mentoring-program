c4f5fd5e9e2e217d7ee2f3849afd04912ad95b61
Vadzim Kurdzesau <VKurdzesau@slb.com>
2022-10-16 20:11:55 +0200

    feat: complete second OOP task

---
+++ OOPFundamentals/DocumentLibrary/Models/Magazine.cs
@@ -0,0 +1,13 @@
+﻿namespace DocumentLibrary.Models;
+
+public class Magazine : Document
+{
+    public string Publisher { get; set; }
+
+    public string ReleaseNumber { get; set; }
+
+    public override string GetInfo()
+    {
+        return $"{DocumentId}, Title: '{Title}', {PublicationDate.ToString("dd MMMM yyyy")}, Release Number: {ReleaseNumber}, Publisher: {Publisher}";
+    }
+}
